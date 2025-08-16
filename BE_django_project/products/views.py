462b0f01516a9df2f6a9a7a0cd3883986ee696c9
Vohiro <dvohiro@gmail.com>
2025-08-16 22:59:24 +0100

    enhanced crud functionality on product and documented readme

--- BE_django_project/products/views.py
+++ BE_django_project/products/views.py
@@ -3,11 +3,21 @@ from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 from django_filters import rest_framework
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
 
 class ListProduct(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    # Pagination defined here (local to this view)
+    class ProductPagination(PageNumberPagination):
+        page_size = 10
+        page_size_query_param = 'page_size'
+        max_page_size = 100
+
+    pagination_class = ProductPagination 
+    
 
     # Enabling filtering, searching and ordering
     filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
