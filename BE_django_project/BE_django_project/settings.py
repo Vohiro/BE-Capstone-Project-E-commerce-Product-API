a348d83ac2b799fa830458ee30e4df7f1a9911bd
Vohiro <dvohiro@gmail.com>
2025-08-27 22:28:08 +0100

    added a yaml file for render hosting

--- BE_django_project/BE_django_project/settings.py
+++ BE_django_project/BE_django_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '.now.sh']
+ALLOWED_HOSTS = ['.onrender.com', '.vercel.app', '127.0.0.1', '.now.sh']
 
 
 # Application definition
