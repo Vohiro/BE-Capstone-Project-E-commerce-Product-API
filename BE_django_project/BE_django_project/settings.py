410486558d755745e189a17ad2636fb8aa1bd1c7
Vohiro <dvohiro@gmail.com>
2025-08-27 23:17:43 +0100

    loaded static files

--- BE_django_project/BE_django_project/settings.py
+++ BE_django_project/BE_django_project/settings.py
@@ -166,7 +166,8 @@ REST_FRAMEWORK = {
         # "rest_framework.authentication.TokenAuthentication"
         ],
     'DEFAULT_PERMISSION_CLASSES': [""
-        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+        "rest_framework.authentication.SessionAuthentication"
         ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
